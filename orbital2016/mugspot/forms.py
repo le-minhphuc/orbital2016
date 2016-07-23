9018ca2ff8a6d6ec85e44fa5aca39c4fab4edd53
Le Minh Phuc <leminhphuc@Les-MacBook-Air.local>
2016-07-23 14:00:19 +0700

    Polishing up 1

--- orbital2016/mugspot/forms.py
+++ orbital2016/mugspot/forms.py
@@ -3,11 +3,11 @@ from .models import Person
 from django.contrib.auth.models import User
 
 class UserRegisterForm(forms.Form):
-	username = forms.CharField(label='User name', max_length=200)
-	user_email = forms.EmailField(label='Email', max_length=254)
-	user_faculty = forms.CharField(label='Faculty', max_length=200)
-	user_password = forms.CharField(label='Password', widget=forms.PasswordInput(), max_length=200)
-	user_password_re = forms.CharField(label='Retype Password', widget=forms.PasswordInput(), max_length=200)
+	username = forms.CharField(label='User name',widget=forms.TextInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;'}), max_length=200)
+	user_email = forms.EmailField(label='Email',widget=forms.TextInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;'}), max_length=254)
+	user_faculty = forms.CharField(label='Faculty',widget=forms.TextInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;'}), max_length=200)
+	user_password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;border:0px;'}), max_length=200)
+	user_password_re = forms.CharField(label='Retype Password', widget=forms.PasswordInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;border:0px;'}), max_length=200)
 
 	def clean(self):
 		super(UserRegisterForm, self).clean()
@@ -23,8 +23,8 @@ class UserRegisterForm(forms.Form):
 		return self.cleaned_data # Why does this work????
 
 class UserLoginForm(forms.Form):
-	user_name = forms.CharField(label='Name',widget=forms.TextInput(attrs={'size':'40'}), max_length=200)
-	user_password = forms.CharField(label='Password', widget=forms.PasswordInput(), max_length=200)
+	user_name = forms.CharField(label='Name',widget=forms.TextInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;'}), max_length=200)
+	user_password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'style': 'width:100%;height:20px;border-radius:30px;border:0px;'}), max_length=200)
 
 class AccountDetailForm(forms.Form):
 	user_name = forms.CharField(label='Username', max_length=200)
@@ -35,13 +35,13 @@ class AccountDetailForm(forms.Form):
 		fields = ['user_name', 'user_email', 'user_faculty']
 		widgets = {
 			'user_name': forms.TextInput(
-					attrs={'id':'User-username', 'required':False}
+					attrs={'id':'User-username', 'required':False, 'style': 'width:100%;height:20px;border-radius:30px;border:0px;',}
 				),
 			'user_email': forms.EmailInput(
-					attrs={'id':'User-useremail', 'required':False}
+					attrs={'id':'User-useremail', 'required':False, 'style': 'width:100%;height:20px;border-radius:30px;border:0px;',}
 				),
 			'user_faculty': forms.TextInput(
-					attrs={'id':'User-userfaculty', 'required':False}
+					attrs={'id':'User-userfaculty', 'required':False, 'style': 'width:100%;height:20px;border-radius:30px;border:0px;',}
 				),
 		}
 
